edbbfb8a12f2b0c2f533bfc142b5f094efe3f712
Gilson Gabriel <gilson@codemastersolucoes.com>
2018-09-04 17:58:58 -0300

    implement searchParams function

--- js/negociacao.js
+++ js/negociacao.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
-    let searchParams = new URLSearchParams(window.location.search);
-
-    if (searchParams.has('SelectedMenuIDKey')) {
-        let param = searchParams.get('SelectedMenuIDKey');
-
-        if (param === 'mnNegociacao') {
+        if (searchParams('SelectedMenuIDKey', 'mnNegociacao')) {
             let $rowsDebits = $('.dxgvDataRow_Edu');
             console.log($rowsDebits);
         }
-    }
 });
+
+let searchParams = function ($key, $value) {
+    let params = new URLSearchParams(window.location.search);
+
+    if (params.has($key)) {
+        let param = searchParams.get($key);
+
+        if (param === $value) {
+            return param;
+        }
+
+        return null;
+    }
+};

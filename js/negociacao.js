3c815a929db00d2d4cf4f00427a7279e7ac4df60
Gilson Gabriel <gilson@codemastersolucoes.com>
2018-09-04 19:01:31 -0300

    implement get due column

--- js/negociacao.js
+++ js/negociacao.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     if (searchParams('SelectedMenuIDKey', 'mnNegociacao')) {
         let $rowsDebits = $('.dxgvDataRow_Edu');
         let $columnDue = $($rowsDebits).last('td');
-        console.log($columnDue);
+        console.log($rowsDebits, $columnDue);
     }
 });
 

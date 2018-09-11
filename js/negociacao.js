f9d461b1bcd9ca94f7ed874a23b620724b4afd02
Gilson Gabriel <gilson@codemastersolucoes.com>
2018-09-11 13:01:30 -0300

    Split date

--- js/negociacao.js
+++ js/negociacao.js
@@ -13,6 +13,12 @@ $(document).ready(function () {
             console.log($dateNow);
             console.log($difDaysDates);
 
+            var date1 = new Date("7/13/2010");
+            var date2 = new Date("12/15/2010");
+            var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+            var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            console.log(diffDays);
+
             // if (()
         });
 

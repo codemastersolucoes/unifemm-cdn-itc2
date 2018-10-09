ab1eed40ba513da229716c7514976e05644fde31
Gilson Gabriel <gilson@codemastersolucoes.com>
2018-10-09 18:58:21 -0300

    add rematricula.js

--- js/rematricula.js
+++ js/rematricula.js
@@ -1,3 +1,12 @@
+(function() {
+    var ev = new $.Event('display'),
+        orig = $.fn.css;
+    $.fn.css = function() {
+        orig.apply(this, arguments);
+        $(this).trigger(ev);
+    }
+})();
+
 $(document).ready(function () {
     let lblCourse = $('.EduLabel:contains("Curso:")').parent();
     let course = $(lblCourse).find('span:eq(1)').text();
@@ -9,7 +18,7 @@ $(document).ready(function () {
         //     $('#EduMatriculaMainTable').empty().html('');
         // });3
 
-        $('#ctl23_xpcPopUp_PW0').isvisible(function() {
+        $('#ctl23_xpcPopUp_PW0').bind('display', function() {
             alert("do something");
         });
     }

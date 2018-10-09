7fe697fe50774ce367bf52aa5aedf1560b8eab8c
Gilson Gabriel <gilson@codemastersolucoes.com>
2018-10-09 17:39:40 -0300

    add rematricula.js

--- js/rematricula.js
+++ js/rematricula.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
     let course = $(lblCourse).find('span:eq(1)').text();
 
     if ((course === 'Ensino Fundamental') || (course === 'Ensino Médio')) {
-        $('#gpAltPlano').parent().hide();
+        // $('#gpAltPlano').parent().hide();
     }
 
-    $('#ctl23_xpcPopUp_PW0').on('show', function () {
+    $('span:contains("Confirmar matrícula")').on('click', function () {
         alert('teste');
     });
 });

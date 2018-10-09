6d4e8bd1dc25cd9b3e1b95c8fcbf47b10ac809c2
Gilson Gabriel <gilson@codemastersolucoes.com>
2018-10-09 17:05:30 -0300

    add rematricula.js

--- js/rematricula.js
+++ js/rematricula.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     let lblCourse = $('.EduLabel:contains("Curso:")').parent();
     let course = $(lblCourse).find('span:eq(1)').text();
 
-    if ((course === 'Ensino Fundamental') || (course === 'Ensino Média')) {
-        console.log(course);
+    if ((course === 'Ensino Fundamental') || (course === 'Ensino Médio')) {
+        $('#gpAltPlano').parent().hide();
     }
 });

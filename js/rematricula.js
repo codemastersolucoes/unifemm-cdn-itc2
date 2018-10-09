fbe80974a8e1b4ba56f61e945a29468d876a40a2
Gilson Gabriel <gilson@codemastersolucoes.com>
2018-10-09 16:50:23 -0300

    add rematricula.js

--- js/rematricula.js
+++ js/rematricula.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    let lblCourse = $('.EduLabel:contains("Curso:")').parent();
+    console.log(lblCourse);
     let course = $('.dxeBase.EduTexto').text();
     console.log(course);
 });
